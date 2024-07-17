684acd4fdf957d11516e69604813901a6a248c7e
Thomas Amar <hello@ebabel.eu>
2024-07-17 18:16:12 +0200

    Read the content of uploaded files

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,11 +1,42 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+import path from 'path';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    text: "EverQuest Inventory",
-    link: {
-      endpoint: "/api/upload",
-      method: "POST",
+  const uploadsDirPath = path.resolve('.', 'public', 'uploads');
+
+  let txtFiles: string[] = [];
+
+  try {
+    // Read the directory contents
+    const files = fs.readdirSync(uploadsDirPath);
+
+    txtFiles = files.filter(file => file.endsWith('-Inventory.txt'));
+  } catch (error) {
+    console.error('Error reading directory:', error);
+  }
+
+  const filesContent: string[] = [];
+
+  txtFiles.map(filename => {
+    const path = `${uploadsDirPath}/${filename}`;
+
+    // Check if the file exists
+    if (!fs.existsSync(path)) {
+      res.status(404).json({ error: 'File not found' });
+      return;
+    }
+  
+    try {
+      const fileContent = fs.readFileSync(path, 'utf8');
+
+      console.log(fileContent);
+
+      filesContent.push(fileContent);
+    } catch (error) {
+      res.status(500).json({ error: 'Error reading file' });
     }
   });
+
+  res.status(200).json(filesContent);
 }
--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,7 +1,7 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import formidable, { IncomingForm } from "formidable";
-import fs from "fs";
-import path from "path";
+import { NextApiRequest, NextApiResponse } from 'next';
+import formidable, { IncomingForm } from 'formidable';
+import fs from 'fs';
+import path from 'path';
 
 export const config = {
   api: {
@@ -9,15 +9,15 @@ export const config = {
   },
 };
 
-const uploadDir: string = path.join(process.cwd(), "/public/uploads");
+const uploadDir: string = path.join(process.cwd(), '/public/uploads');
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method not allowed" });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
   }
 
   const form = new IncomingForm({
@@ -41,20 +41,20 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       if (err) {
         return res
           .status(500)
-          .json({ message: "File upload failed", error: err });
+          .json({ message: 'File upload failed', error: err });
       }
 
       const file: formidable.File | undefined = Array.isArray(files.file)
         ? files.file[0]
         : (files.file as formidable.File | undefined);
       if (file) {
-        const filePath: string = (file as formidable.File).filepath || "";
+        const filePath: string = (file as formidable.File).filepath || '';
 
         return res
           .status(200)
-          .json({ message: "File uploaded successfully", filePath });
+          .json({ message: 'File uploaded successfully', filePath });
       } else {
-        return res.status(400).json({ message: "No file uploaded" });
+        return res.status(400).json({ message: 'No file uploaded' });
       }
     }
   );
